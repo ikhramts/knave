1ecf1b0d5d60d54cad12465e4d67c4397b45a8f1
Iouri Khramtsov <ioukhr@gmail.com>
2017-08-20 17:34:02 -0400

    Fixed issues with dev env

--- src/__tests__/knave.tests.ts
+++ src/__tests__/knave.tests.ts
@@ -5,4 +5,16 @@ describe("Knave", () =>{
     it("should pass", () => {
         expect(true).toBe(true);
     });
+
+    it("Should parse", () => {
+        expect.assertions(2);
+        return Knave.buildFile(inputFile('test1.sql')).then(table => {
+            expect(table).not.toBeNull();
+            expect(table.name).toBe("MyTable");
+        })
+    })
 });
+
+function inputFile(pathFromTests:string) : string {
+    return "src/__tests__/inputs/" + pathFromTests;
+}
--- src/knave.ts
+++ src/knave.ts
@@ -1,15 +1,22 @@
 import { Table } from './model'
 import { parse } from './sql-parser'
 import * as fs from 'fs';
+import * as path from 'path';
 
 export namespace Knave {
     export function buildFile(filePath: string) : Promise<Table> {
         return new Promise((resolve, reject) => {
-            fs.readFile(filePath, (err, data) => {
+            let fullFilePath = path.resolve(filePath);
+            if(!fs.existsSync(fullFilePath)) {
+                throw "File '" + fullFilePath + "' does not exist";
+            }
+
+            fs.readFile(fullFilePath, (err, data) => {
                 if (err)
                     reject (err);
 
-                let model = <Table>parse(data, {});
+                let model = <Table>parse(data.toString(), {});
+                console.log(JSON.stringify(model));
                 resolve(model);
             });
         });
